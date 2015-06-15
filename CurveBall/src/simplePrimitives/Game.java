b4f833cc316fa6f90e5d5e2cc2763f6abeb82341
Nicolas Meisberger <orangensaft@outlook.com>
2015-06-15 13:48:29 +0200

    Code cleanup -loadTexture in GameUtils verschoben -Gittermodell bei start deaktiviert

--- CurveBall/src/simplePrimitives/Game.java
+++ CurveBall/src/simplePrimitives/Game.java
@@ -32,7 +32,6 @@ import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
-
 // matrix Utilities was removed with lwjgl3, we use our own
 import mat.*;
 
@@ -85,7 +84,6 @@ public class Game {
     private Vec3 modelAngle = new Vec3(0,0,0);
     private Vec3 cameraPos = new Vec3(0,0,-5); //Kamera-Position
     private float deltaRot = 5f;
-    private float normalScale=0.1f;
     
     // toggles & interactions
     private boolean showMesh = true;
@@ -225,7 +223,7 @@ public class Game {
         glClearColor(0.3f, 0.3f, 0.3f, 0.0f);
         
         // Switch to wireframe
-        glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+        glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
         // -> back to solid faces: glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
  
         // Backface culling: Shows, if the triangles are correctly defined
@@ -348,68 +346,8 @@ public class Game {
         modelMatrixLocationNormals = GL20.glGetUniformLocation(pNormalsId, "modelMatrix");        
     }
  
-    /**
-     * Uses an external class to load a PNG image and bind it as texture
-     * @param filename
-     * @param textureUnit
-     * @return textureID
-     */
-    private int loadPNGTexture(String filename, int textureUnit) {
-        ByteBuffer buf = null;
-        int tWidth = 0;
-        int tHeight = 0;
-         
-        try {
-            // Open the PNG file as an InputStream
-            InputStream in = new FileInputStream(filename);
-            // Link the PNG decoder to this stream
-            PNGDecoder decoder = new PNGDecoder(in);
-             
-            // Get the width and height of the texture
-            tWidth = decoder.getWidth();
-            tHeight = decoder.getHeight();
-             
-             
-            // Decode the PNG file in a ByteBuffer
-            buf = ByteBuffer.allocateDirect(
-                    4 * decoder.getWidth() * decoder.getHeight());
-            decoder.decode(buf, decoder.getWidth() * 4, Format.RGBA);
-            buf.flip();
-             
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-         
-        // Create a new texture object in memory and bind it
-        int texId = GL11.glGenTextures();
-        GL13.glActiveTexture(textureUnit);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texId);
-         
-        // All RGB bytes are aligned to each other and each component is 1 byte
-        GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
-         
-        // Upload the texture data and generate mip maps (for scaling)
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, tWidth, tHeight, 0, 
-                GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
-        GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
-         
-        // Setup the ST coordinate system
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
-         
-        // Setup what to do when the texture has to be scaled
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, 
-                GL11.GL_NEAREST);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, 
-                GL11.GL_LINEAR_MIPMAP_LINEAR);
-
-        return texId;
-    }
-    
     private void setupTextures() {
-        textureID = this.loadPNGTexture("assets/gdv.png", GL13.GL_TEXTURE0);
+        textureID = GameUtils.loadPNGTexture("assets/gdv.png", GL13.GL_TEXTURE0);
     }
     
     private void initObject(){
@@ -492,9 +430,9 @@ public class Game {
 			normalLines[pos++]=vertices[i];
 			normalLines[pos++]=vertices[i+1];
 			normalLines[pos++]=vertices[i+2];
-			normalLines[pos++]=vertices[i]+normalScale*normals[i];
-			normalLines[pos++]=vertices[i+1]+normalScale*normals[i+1];
-			normalLines[pos++]=vertices[i+2]+normalScale*normals[i+2];
+			normalLines[pos++]=vertices[i]+GameUtils.normalScale*normals[i];
+			normalLines[pos++]=vertices[i+1]+GameUtils.normalScale*normals[i+1];
+			normalLines[pos++]=vertices[i+2]+GameUtils.normalScale*normals[i+2];
 		}
 		FloatBuffer normalLinesBuffer = BufferUtils.createFloatBuffer(normalLines.length);
 		normalLinesBuffer.put(normalLines);
--- CurveBall/src/simplePrimitives/GameUtils.java
+++ CurveBall/src/simplePrimitives/GameUtils.java
@@ -1,5 +1,17 @@
 package simplePrimitives;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL30;
+
+import de.matthiasmann.twl.utils.PNGDecoder;
+import de.matthiasmann.twl.utils.PNGDecoder.Format;
+
 
 /**
  * Diverse globale Methoden/Variablen
@@ -9,7 +21,7 @@ package simplePrimitives;
 public abstract class GameUtils {
 	
 	public static enum Sides {left,right,top,bottom,none};
-	
+	public static float normalScale=0.2f;
 	//View-Volume
 	public static float near=-1f;
 	public static float far=1f;
@@ -17,4 +29,65 @@ public abstract class GameUtils {
 	public static float right=1f;
 	public static float bottom=-1f;
 	public static float top=1f;
+	
+	 /**
+     * Uses an external class to load a PNG image and bind it as texture
+     * @param filename
+     * @param textureUnit
+     * @return textureID
+     */
+    public static int loadPNGTexture(String filename, int textureUnit) {
+        ByteBuffer buf = null;
+        int tWidth = 0;
+        int tHeight = 0;
+         
+        try {
+            // Open the PNG file as an InputStream
+            InputStream in = new FileInputStream(filename);
+            // Link the PNG decoder to this stream
+            PNGDecoder decoder = new PNGDecoder(in);
+             
+            // Get the width and height of the texture
+            tWidth = decoder.getWidth();
+            tHeight = decoder.getHeight();
+             
+             
+            // Decode the PNG file in a ByteBuffer
+            buf = ByteBuffer.allocateDirect(
+                    4 * decoder.getWidth() * decoder.getHeight());
+            decoder.decode(buf, decoder.getWidth() * 4, Format.RGBA);
+            buf.flip();
+             
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+         
+        // Create a new texture object in memory and bind it
+        int texId = GL11.glGenTextures();
+        GL13.glActiveTexture(textureUnit);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texId);
+         
+        // All RGB bytes are aligned to each other and each component is 1 byte
+        GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
+         
+        // Upload the texture data and generate mip maps (for scaling)
+        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, tWidth, tHeight, 0, 
+                GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
+        GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+         
+        // Setup the ST coordinate system
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
+         
+        // Setup what to do when the texture has to be scaled
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, 
+                GL11.GL_NEAREST);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, 
+                GL11.GL_LINEAR_MIPMAP_LINEAR);
+
+        return texId;
+    }
+	
 }
